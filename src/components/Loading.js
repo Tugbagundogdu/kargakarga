2b2a65f5892e167ca9707847e07c3a70f364d95c
tugbagundogdu <78304413+Tugbagundogdu@users.noreply.github.com>
2024-04-05 08:53:39 +0300

    loading added

---
+++ src/components/Loading.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Loading = () => {
+  return (
+    <div className='flex justify-center items-center h-screen text-gray-800 text-3xl '>
+      Loading...
+    </div>
+  )
+}
+
+export default Loading
